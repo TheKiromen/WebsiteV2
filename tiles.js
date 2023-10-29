3df8e62c346f5ff231b9903f2a0de0acbd65cde9
Dominik Kruczek <TheKiromen@gmail.com>
2023-10-29 22:59:00 +0100

    Linking and styling

---
+++ tiles.js
@@ -0,0 +1,14 @@
+const wrapper = document.getElementById("banner");
+
+let tmp = () => {
+    wrapper.innerHTML="Hello World";
+}
+
+// TODO:
+// Set height to be proportional to width (1.61  w/h ratio?)
+// Make banner background an animated gradient (angled 45deg?)
+// Generate the tiles
+// Style the tiles as in video
+// Make them interactive
+// Figure out how to make them into a logo??
+// Try to not break up animation when clicking multiple times??
