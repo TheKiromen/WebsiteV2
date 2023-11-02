642f5c42ba5bb5defe32240698b4a31b788188ea
Dominik Kruczek <TheKiromen@gmail.com>
2023-11-02 17:09:44 +0100

    Added dynamic sizing to tiles

--- tiles.js
+++ tiles.js
@@ -1,4 +1,6 @@
-let cols = Math.floor(document.body.clientWidth / 50);
+let tileSize = Math.floor(document.body.clientWidth / 50);
+
+let cols = Math.floor(document.body.clientWidth / tileSize);
 let rows = Math.ceil(cols/3);
 
 const wrapper = document.getElementById("banner");
@@ -18,8 +20,9 @@ const handleOnClick = index => {
     anime({
         targets: '.tile',
         backgroundColor: colors[count % colors.length],
-        delay: anime.stagger(50, {
-            grid: [cols, rows]
+        delay: anime.stagger(10, {
+            grid: [cols, rows],
+            from: index
         })
       });
 }
@@ -33,6 +36,7 @@ const createTile = (index) => {
 }
 
 const createTiles = (quantity) => {
+    wrapper.style.height = rows * tileSize + "px";
     // Generate list of tile nodes
     Array.from(Array(quantity)).map((tile,index) => {
         wrapper.appendChild(createTile(index));
@@ -44,7 +48,8 @@ const createGrid = () => {
     wrapper.innerHTML = "";
 
     // Recalculate col and row count
-    cols = Math.floor(document.body.clientWidth / 50);
+    tileSize = Math.ceil(document.body.clientWidth / 50);
+    cols = Math.floor(document.body.clientWidth / tileSize);
     rows = Math.ceil(cols/3); 
 
     // Update css vars
@@ -54,12 +59,11 @@ const createGrid = () => {
     createTiles(cols * rows);
 }
 
+
 window.onresize = () => createGrid();
 createTiles(cols * rows);
 
 // TODO:
 // Make banner background an animated gradient (angled 45deg?)
 // Style the tiles as in video
-// Make them interactive
 // Figure out how to make them into a logo??
-// Try to not break up animation when clicking multiple times??
