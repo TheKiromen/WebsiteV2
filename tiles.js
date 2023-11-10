f81643b85f21a14779860ad40d3a6bfb8ed5f236
Dominik Kruczek <TheKiromen@gmail.com>
2023-11-11 00:52:30 +0100

    First part of the logo

--- tiles.js
+++ tiles.js
@@ -8,7 +8,12 @@ wrapper.style.setProperty("--rows", rows);
 
 // TODO: Fill with apropriate indexes to create logo
 // Middle of the canvas (25, 8), where 8 = index 433 ((51*8)+25)
-const logoTiles = [433];
+
+// Smaller Version
+// const logoTiles = [433,432,381,483,330,534,279,585,278,277,276,584,583,582,531,327,326,377,428,479,530,273,324,579,528,527,323,374,373,476,475,424,423];
+
+// Larger Version 
+const logoTiles = [433,432,381,330,279,228,483,534,585,636,227,226,635,634,633,582,225,276,275,326,376,530,581,529,478,427,325,221,272,629,578,577,526,525,474,473,271,322,321,372,371,422,421];
 let logoIndexes = [];
 
 let toggled = false;
