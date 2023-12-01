704f78fd0443c633a581f66728e2be06504a7290
Dominik Kruczek <TheKiromen@gmail.com>
2023-12-01 21:34:16 +0100

    Hoover animation

--- footer.js
+++ footer.js
@@ -4,10 +4,26 @@ let linkedin = document.getElementById("linkedin");
 
 const onMouseEnter = e => {
     let icon = e.srcElement.childNodes[1];
-    // TODO: start and animation?
+    anime({
+        targets:icon,
+        rotate: 360
+    })
+    console.log(icon);
+}
+
+const onMouseLeave = e => {
+    let icon = e.srcElement.childNodes[1];
+    anime({
+        targets:icon,
+        rotate: 0
+    })
     console.log(icon);
 }
 
 github.addEventListener("mouseenter", onMouseEnter);
 mail.addEventListener("mouseenter", onMouseEnter);
 linkedin.addEventListener("mouseenter", onMouseEnter);
+
+github.addEventListener("mouseleave", onMouseLeave);
+mail.addEventListener("mouseleave", onMouseLeave);
+linkedin.addEventListener("mouseleave", onMouseLeave);
