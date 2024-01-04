afdc190f7ef79a045fd2c606d7203b482bb9d2b9
Dominik Kruczek <TheKiromen@gmail.com>
2024-01-04 20:43:11 +0100

    Heading color animation

--- content.js
+++ content.js
@@ -9,13 +9,25 @@ const ContentOnMouseEnter = e => {
     let title = content[section][1].childNodes[1].childNodes;
     // Get subsection title letters
     let animationTargets = Array.prototype.slice.call(title).slice(1,title.length-1);
-    // console.log(animationTargets);
-    // TODO: Check if prev target section == current, if yes dont animate again
+    anime({
+        targets: animationTargets,
+        color: "#00f4ff",
+        // color: "#00ADB5",
+        delay: anime.stagger(10)
+    })
 }
 
 const ContentOnMouseLeave = e => {
     // Get target subsection
-    let section = e.target.id.split("_")[0]; 
+    let section = e.target.id.split("_")[0];
+    let title = content[section][1].childNodes[1].childNodes;
+    // Get subsection title letters
+    let animationTargets = Array.prototype.slice.call(title).slice(1,title.length-1);
+    anime({
+        targets: animationTargets,
+        color: "#EEEEEE",
+        delay: anime.stagger(10)
+    })
 }
 
 for(var key in content){
