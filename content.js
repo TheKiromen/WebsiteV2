eea60383bc265d2e7e30f555d3695301b644ac66
Dominik Kruczek <TheKiromen@gmail.com>
2024-01-04 16:46:33 +0100

    Code refactor

--- content.js
+++ content.js
@@ -1,29 +1,25 @@
 let cooking = [document.getElementById("cooking_image"), document.getElementById("cooking_description")];
 let books = [document.getElementById("books_image"), document.getElementById("books_description")];
 let programming = [document.getElementById("programming_image"), document.getElementById("programming_description")];
+let content = {"cooking":cooking, "books":books, "programming":programming};
 
-// TODO: Optimize and refactor?
 const ContentOnMouseEnter = e => {
     // Get target subsection
     let section = e.target.id.split("_")[0];
-    console.log(section);
+    // Get subsection title letters
+    let animationTargets = content[section][1];
+    console.log(animationTargets);
+    // TODO: Check if prev target section == current, if yes dont animate again
 }
 
 const ContentOnMouseLeave = e => {
     // Get target subsection
-    let section = e.target.id.split("_")[0];
-    console.log(section);
+    let section = e.target.id.split("_")[0]; 
 }
 
-cooking.forEach(element => {
-    element.addEventListener("mouseenter", ContentOnMouseEnter);
-    element.addEventListener("mouseleave", ContentOnMouseLeave);
-});
-books.forEach(element => {
-    element.addEventListener("mouseenter", ContentOnMouseEnter);
-    element.addEventListener("mouseleave", ContentOnMouseLeave);
-});
-programming.forEach(element => {
-    element.addEventListener("mouseenter", ContentOnMouseEnter);
-    element.addEventListener("mouseleave", ContentOnMouseLeave);
-});
+for(var key in content){
+    content[key].forEach(el => {
+        el.addEventListener("mouseenter", ContentOnMouseEnter);
+        el.addEventListener("mouseleave", ContentOnMouseLeave);
+    })
+}
